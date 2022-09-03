797b2083f3a247ab7a029e20d4cdded4e532c4b4
Volodymyr <heravolodymyr1991@gmail.com>
2022-09-03 14:36:22 +0300

    added dto

---
+++ DriveMoto/Models/AddProductRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriveMoto.Models
+{
+    public class AddProductRequest
+    {
+        [Required] //атрибут [Required] вказує на те що поля повині бути
+                   //обовязково заповненими
+        public string? Name { get; set; }
+        [Required]
+        public int СodeProduct { get; set; }
+        [Required]
+        public string? Сategory { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public double Discount { get; set; }
+    }
+}
---
+++ DriveMoto/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DriveMoto.Models
+{
+    public class Client
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required] 
+        public string? LastName { get; set; }
+        [Required]
+        public int Phone { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+    }
+}
